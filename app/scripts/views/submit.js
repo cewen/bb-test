cf995ef95fc1be5fe96efaeb1197f1a35fb0e58a
Chris Ewen <christopher.ewen@codeandtheory.com>
2014-01-24 15:41:30 -0500

    Added quotes around dish name in suggestion text

--- app/scripts/views/submit.js
+++ app/scripts/views/submit.js
@@ -1,7 +1,7 @@
 IIAS.Views.Submit = Backbone.View.extend({
 	className: 'submission-text',
 	
-	template: _.template('Think <%= name %> is a salad?&nbsp;&nbsp;<button type="button" class="btn btn-primary btn-xs">Nominate!</button>'),
+	template: _.template('Think "<%= name %>" is a salad?&nbsp;&nbsp;<button type="button" class="btn btn-primary btn-xs">Nominate!</button>'),
 	
 	render: function(){
 		this.$el.html(this.template(this.model.attributes));
